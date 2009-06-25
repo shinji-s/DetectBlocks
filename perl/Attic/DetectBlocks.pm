23fbc814a568e550e633596b1378ebb120608594
Tomohiro Shibuta <shibuta@(none)>
2009-06-25 15:58:05 +0000

    add recheckblock

--- perl/Attic/DetectBlocks.pm
+++ perl/Attic/DetectBlocks.pm
@@ -85,24 +85,133 @@ sub dblocks_saiki{
 	}
 
     }else{
-
-	my $kk = $this->{blockarr};
-	my @kariblockarr = @$kk;
-	push(@kariblockarr,[]);
-	$this->{blockarr} = \@kariblockarr;
-	my $maxlen = $this->block_saiki($sourceelem);
-
-	if($maxlen < 5){
-	    pop(@kariblockarr);
+	
+	for my $i($this->recheckblock($sourceelem)){
+#for my $i(($sourceelem)){
+	    my $kk = $this->{blockarr};
+	    my @kariblockarr = @$kk;
+	    push(@kariblockarr,[]);
 	    $this->{blockarr} = \@kariblockarr;
-	}else{
-	    my $blockarr = $this->{blockarr};
-	    my @blockarr = @$blockarr;
-	    my @block = $blockarr[$#blockarr];
-	    $this->writeblocktype(@block, $sourceelem);
-	}	
-
+	    my $maxlen = $this->block_saiki($i);
+	    if($maxlen < 5){
+		pop(@kariblockarr);
+		$this->{blockarr} = \@kariblockarr;
+	    }else{
+		my $blockarr = $this->{blockarr};
+		my @blockarr = @$blockarr;
+		my @block = $blockarr[$#blockarr];
+		$this->writeblocktype(@block, $i);
+	    }	
+	    
+	}
     }
+}
+
+
+sub recheckblock{
+    my ($this, $sourceelem) = @_;
+
+    my $elem = ${$sourceelem};
+
+    my @aarr = $elem->find("a");
+    my $allanum = $#{aarr} + 1;
+    my @farr = $elem->find("form");
+    my $allformnum = $#{farr} + 1;
+    my @tarr = $elem->find("~text");
+    my $alltextnum = $#{tarr} + 1;
+    my $allfootnum = 0;
+    my $allmaintnum = 0;
+    for my $text(@tarr){
+	$allfootnum += 1 if($text->attr("text") =~ /住所|所在地|郵便番号|電話番号|著作権|問[い]?合[わ]?せ|利用案内|質問|意見|\d{3}\-?\d{4}|Tel|TEL|.+[都道府県].+[市区町村]|(06|03)\-?\d{4}\-?\d{4}|\d{3}\-?\d{3}\-?\d{4}|mail|Copyright|©|(c)|著作権/);
+	$allmaintnum += 1 if($text->attr("text") =~ /。|、|ます|です|でした|ました/);
+    }
+
+    my @rearr =();
+
+    $this->recheckblock_saiki($sourceelem, \@rearr, $allanum, $allformnum, $alltextnum, $allfootnum, $allmaintnum);
+
+    return @rearr;
+
+}
+
+
+sub recheckblock_saiki{
+    my ($this, $sourceelem, $rearr, $aa, $af, $at, $af, $amt) = @_;
+
+    my $elem = ${$sourceelem};
+
+    if($at<=3){
+	push(@{$rearr}, $sourceelem);
+	return 0;
+    }
+
+    my $childrenflag = 0;
+    my @kariarr;
+    for my $child($elem->content_list){
+	my @aarr = $child->find("a");
+	my $anum = $#{aarr} + 1;
+	my @farr = $child->find("form");
+	my $formnum = $#{farr} + 1;
+	my @tarr = $child->find("~text");
+	my $textnum = $#{tarr} + 1;
+	my $footnum = 0;
+	my $maintnum = 0;
+	for my $text(@tarr){
+	    $footnum += 1 if($text->attr("text") =~ /住所|所在地|郵便番号|電話番号|著作権|問[い]?合[わ]?せ|利用案内|質問|意見|\d{3}\-?\d{4}|Tel|TEL|.+[都道府県].+[市区町村]|(06|03)\-?\d{4}\-?\d{4}|\d{3}\-?\d{3}\-?\d{4}|mail|Copyright|©|(c)|著作権/);
+	    $maintnum += 1 if($text->attr("text") =~ /。|、|ます|です|でした|ました|だった/);
+	}
+
+#	if($anum/$aa>0.9 || $formnum/$af>0.9 || $textnum/$at>0.9 || $footnum/$af>0.9 || $maintnum/$amt>0.9){
+#	    push(@rearr, \$child);
+#	}
+
+	my $flag = 0;
+	$flag += 1 if($aa != 0 && $anum/$aa>0.8);
+	$flag += 1 if($af != 0 && $formnum/$af>0.8);
+	$flag += 1 if($at != 0 && $textnum/$at>0.8);
+	$flag += 1 if($af != 0 && $footnum/$af>0.8);
+	$flag += 1 if($amt != 0 && $maintnum/$amt>0.8);
+#	print $anum/$aa,"\n",$child->find("~text"),"\n\n"if($aa!=0);
+	if($flag == 1){
+	    for my $karichild(@kariarr){
+		push(@{$rearr}, $karichild);
+	    }
+	    @kariarr =();
+	    push(@{$rearr}, \$child);
+	    $childrenflag +=1;
+	}elsif($flag >=2){
+	    for my $karichild(@kariarr){
+		push(@{$rearr}, $karichild);
+	    }
+	    @kariarr = ();
+	    $this->recheckblock_saiki(\$child, $rearr, $aa, $af, $at, $af, $amt);
+	    $childrenflag += 1;
+	}elsif($childrenflag == 0){
+	    push(@kariarr, \$child);
+	}else{
+	    push(@{$rearr}, \$child);
+	}
+=comment
+	if($flag > 0){
+	    for my $karichild(@kariarr){
+		push(@{$rearr}, $karichild);
+	    }
+	    @kariarr = ();
+	    push(@{$rearr}, \$child);
+	    $childrenflag += 1;
+	}elsif($childrenflag != 0){
+	    push(@{$rearr}, \$child);
+	}else{
+	    push(@kariarr, \$child);
+	}
+=cut
+    }
+
+    if($childrenflag == 0){
+	push(@{$rearr}, $sourceelem);
+	return 0;
+    }
+    return 1;
 }
 
 
@@ -300,6 +409,7 @@ sub checkfoot{
 
     my $total = $#block + 1;
     my $karifootnum = 0;
+    my $karitextlen = 0;
     my $textlen = 0;
     for my $eachleaf(@block){
 	my @eachleaf = @$eachleaf;
@@ -307,12 +417,19 @@ sub checkfoot{
 
 	next if($text =~ /^[\s　]+$/ || $text eq "");
 
-	$karifootnum += 1 if($text =~ /住所|所在地|郵便番号|電話番号|著作権|問[い]?合[わ]?せ|利用案内|質問|意見|\d{3}\-?\d{4}|Tel|TEL|.+[都道府県].+[市区町村]|(06|03)\-?\d{4}\-?\d{4}|\d{3}\-?\d{3}\-?\d{4}|mail/);
+	if($text =~ /住所|所在地|郵便番号|電話番号|著作権|問[い]?合[わ]?せ|利用案内|質問|意見|\d{3}\-?\d{4}|Tel|TEL|.+[都道府県].+[市区町村]|(06|03)\-?\d{4}\-?\d{4}|\d{3}\-?\d{3}\-?\d{4}|mail|Copyright|©|(c)|著作権/){
+	    $karifootnum += 1;
+	    $karitextlen += length($text);
+	}
 	$textlen += length($text);
     }
 
-    if($karifootnum / $total >= 0.5 && $textlen / $this->{alltextlen} < 0.3){
-	return 1;
+    if(($karifootnum / $total >= 0.5 || $karitextlen/$textlen > 0.8) && $textlen / $this->{alltextlen} < 0.3){
+	if($this->checkcopy($block)){
+	    return 2;
+	}else{
+	    return 1;
+	}
     }else{
 	return 0;
     }
@@ -327,13 +444,11 @@ sub checkmaintext{
     my $total = $#block + 1;
     my $karimainnum = 0;
     my $karimainlen = 0;
-    my $totallen = 0;
     my $textlen = 0;
     for my $eachleaf(@block){
         my @eachleaf = @$eachleaf;
         my $text = $eachleaf[1];
 
-	$totallen += length($text);
         next if($text =~ /^[\s　]+$/ || $text eq "");
 
         if($text =~ /。|、|ます|です|でした|ました/){
@@ -378,20 +493,16 @@ sub checkcopy{
 }
 
 
-sub writeblocktype{
-    my ($this, $kariblock, $sourceelem) = @_;
+sub checklink{
+    my ($this, $block) = @_;
 
-    my @block = @$kariblock;
-    my $elem = ${$sourceelem};
-
-    return 0 if(@block == []);
+    my @block = @$block;
 
     my $karia = 0;
     my $kariatnum = 0;
     my $karinottnum = 0;
     my $kariahref = 0;
-    my $formnum = 0;
-
+    
     for my $leaf(@block){
 
 	my @leaf = @$leaf;
@@ -412,33 +523,81 @@ sub writeblocktype{
 	    $karinottnum += length($text);
 	}
 
-	$formnum += 1 if($this->assoc(\@tagarr, "form") != -1);
+    }
+
+    if($karia/($#block+1) >= 0.5 || $karinottnum < $kariatnum ){
+        if(defined($this->{domain})){
+            if($kariahref/$karia >= 0.5){
+                return 1;
+            }else{
+		return 2;
+            }
+        }else{
+	    return 3;
+	}
+    }
+
+    return 0;
+}
+
+
+sub checkform{
+    my ($this, $block) = @_;
+
+    my @block = @$block;
+
+    my $total = $#block + 1;
+    my $kariformnum = 0;
+    my $textlen = 0;
+
+    for my $leaf(@block){
+
+        my @leaf = @$leaf;
+        my @tagarr = @{$leaf[0]};
+        
+	if($this->assoc(\@tagarr,"form") != -1){
+	    $kariformnum += 1;
+	}
 
     }
+
+    if($kariformnum/$total >= 0.8){
+        return 1;
+    }else{
+        return 0;
+    }
+   
+}
+
+
+sub writeblocktype{
+    my ($this, $kariblock, $sourceelem) = @_;
+
+    my @block = @$kariblock;
+    my $elem = ${$sourceelem};
+
+    return 0 if(@block == []);
+
 
     my $typeflag = "";
 #    if($karia/($#block+1) >= 0.5 || $karinottnum/$kariatnum <= 0.3){
-    if($karia/($#block+1) >= 0.5 || $karinottnum < $kariatnum ){
-	if(defined($this->{domain})){
-	    if($kariahref/$karia >= 0.5){
-		$typeflag = "inlink";
-	    }else{
-		$typeflag = "outlink";
-	    }
-	}else{
-	    $typeflag = "link"
+    if((my $lflag = $this->checklink(\@block))){
+	if($lflag == 1){
+	    $typeflag = "link internal";
+	}elsif($lflag == 2){
+	    $typeflag = "link external";
+	}elsif($lflag == 3){
+	    $typeflag = "link";
 	}
     }else{
-	if($formnum/($#block+1) > 0.8){
+	if($this->checkform(\@block)){
 	    $typeflag = "form";
-	}elsif($this->checkfoot(\@block)){
+	}elsif((my $fcflag = $this->checkfoot(\@block))){
 	    $typeflag = "footer";
+	    $typeflag .= " copyright" if($fcflag == 2);
 	}elsif($this->checkmaintext(\@block)){
 	    $typeflag = "maintext";
 	}
-    }
-    if($this->checkcopy(\@block)){
-	$typeflag .= " copyright";
     }
     if($typeflag eq ""){
 	$typeflag = "unknown_text";
