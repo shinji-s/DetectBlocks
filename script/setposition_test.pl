2be00f17eb6d45138d84d437ac07d22a03267a00
Tomohiro Shibuta <shibuta@(none)>
2009-12-16 11:38:29 +0000

    引数を与えるように修正

--- script/setposition_test.pl
+++ script/setposition_test.pl
@@ -7,8 +7,13 @@ use SetPosition;
 
 
 #my $str = "../sample/htmls/Kyoto/001.html";
-my $str = "http://www.google.co.jp";
+#my $str = "http://www.google.co.jp";
+my $str = $ARGV[0];
 
+if ($str eq "") {
+    print "Please input URL or path of HTML file\n";
+    exit();
+}
 
 my $execpath = '../tools/addMyAttrToHtml/staticExe/wkhtmltopdf-reed';
 my $jspath = '../tools/addMyAttrToHtml/staticExe/myExecJs.js';
