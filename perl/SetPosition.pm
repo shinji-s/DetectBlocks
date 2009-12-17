d92292ca830704e82bed4cd5c9de74ee15dbf338
Tomohiro Shibuta <shibuta@(none)>
2009-12-17 17:18:09 +0000

    optionでsource_urlを指定した場合、cssとjavascriptの相対パスを絶対パスに修正するように変更

--- perl/SetPosition.pm
+++ perl/SetPosition.pm
@@ -8,7 +8,7 @@ use POSIX;
 use DetectBlocks;
 
 
-our %opt = ('proxy'=>'http://proxy.kuins.net:8080/', 'nodec'=>1);
+our %thisopt = ('proxy'=>'http://proxy.kuins.net:8080/', 'nodec'=>1);
 
 # 子プロセスが終了するまで待つ時間の最大値(秒)
 our $loopLimit = 30;
@@ -80,7 +80,13 @@ sub execAddPosition {
 # ファイル、URL、文字列
 sub setPosition {
 
-    my ($target, $execpath, $jspath) = @_;
+    my ($target, $execpath, $jspath, $opt) = @_;
+
+    while (my ($key, $val) = each(%thisopt)) {
+	unless (defined($opt->{$key})) {
+	    $opt->{$key} = $val;
+	}
+    }
 
     my $html;
     # ローカルのファイルの場合
@@ -90,16 +96,77 @@ sub setPosition {
 	close(FILE);
     }
     # urlの場合
-    elsif ($target =~ /^http:/) {
+    elsif ($target =~ /^http:\/\//) {
 	my $DetectBlocks = new DetectBlocks();
-	($html, $target) = $DetectBlocks->Get_Source_String($target, \%opt);
+	($html, $target) = $DetectBlocks->Get_Source_String($target, $opt);
     }
     # その他はhtml文字列として扱う場合
     else {
 	$html = $target;
     }
+# $urlが指定されている場合、相対パスを絶対パスにする
+    if ($opt->{'source_url'}) {
+	$html = &SetPosition::rel_path2abs_path($html, $opt->{'source_url'});
+    }
 
     return &SetPosition::execAddPosition($html, $execpath, $jspath);
+}
+
+
+# cssとjavascriptが相対パスで指定されている場合、絶対パスに変換する
+# 入力はhtmlの文字列
+sub rel_path2abs_path {
+
+    my ($html, $url) = @_;
+
+    # typeがcssとjavascriptのタグを収集
+    my @elems;
+    while($html =~ /<[^<^>]*type\=.text\/(css|javascript)[^<^>]*>/ig){
+	push(@elems, $&);
+    }
+    # hrefかsrcの値を絶対パスにして置換する
+    foreach my $elemText (@elems) {
+	if ($elemText =~ /(src|href)\=[\'\"](.*?)[\'\"]/i) {
+	    my $relPath = $2;
+	    my $absPath = &rel2abs_http($url, $relPath);
+	    next if ($absPath == -1);
+	    my $newText = $elemText;
+	    $newText =~ s/\Q$relPath\E/$absPath/;
+	    $html =~ s/\Q$elemText\E/$newText/;
+	}
+    }
+    return $html;
+}
+
+# httpの絶対パスに変換
+sub rel2abs_http {
+
+    my ($url, $path) = @_;
+    
+    my $domain;
+    if ($url =~ /^(http\:\/\/[^\/]+)/) {
+	$domain = $1;
+    } else {
+	return -1;
+    }
+    my $pos;
+    if ($url =~ /^(http\:\/\/.*\/)[^\/]*$/) {
+	$pos = $1;
+    }
+    my $absPath;
+    if ($path =~ /^http\:\/\//) {
+	$absPath = $path;
+    } elsif ($path =~ /^\/.*/) {
+	$absPath = $domain . $path;
+    } else {
+        $absPath = $pos  . $path;
+    }
+    # /../../みたいなのがうまくいかなかったのでwhile
+    while ($absPath =~ /(\/\.\/|\/\.\.\/)/) {
+	$absPath =~ s/\/\.\///g;
+	$absPath =~ s/\/[^\/]+\/\.\.\//\//g;
+    }
+    return $absPath; 
 }
 
 
